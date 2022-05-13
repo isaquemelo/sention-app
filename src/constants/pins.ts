80119db826b360f70b3fd58dfdd64a33f12cfde2
Isaque Melo <isaque.melo@mercadolivre.com>
2022-05-12 22:02:04 -0300

    feat: Add output pins contant file

---
+++ src/constants/pins.ts
@@ -0,0 +1,27 @@
+import urls from './urls';
+
+const pins = {
+    'ACCEPTED_OUTPUT_PINS': [
+        2,
+        4,
+        13,
+        14,
+        15,
+        16,
+        17,
+        18,
+        19,
+        20,
+        21,
+        22,
+        23,
+        25,
+        26,
+        27,
+        32,
+        33
+    ],
+
+}
+
+export default pins;
