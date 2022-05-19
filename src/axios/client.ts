90f8e232b9574ccf52bc8dbd1850f65fac7d9832
Isaque Melo <isaque.melo@mercadolivre.com>
2022-05-19 19:20:52 -0300

    fix: Use interceptor to avoid async problem

--- src/axios/client.ts
+++ src/axios/client.ts
@@ -1,10 +1,18 @@
 import axios from "axios";
 
-const jsonToken = await window.sessionStorage.getItem('token')
-const token = JSON.parse(<string>jsonToken)
 
-const client = axios.create({
-    headers: { 'Authorization': "" + token }
-});
+
+const client = axios.create({});
+
+client.interceptors.request.use(async (request) => {
+    if (request.headers) {
+        const jsonToken = await window.sessionStorage.getItem('token')
+        const token = JSON.parse(<string>jsonToken)
+
+        request.headers['Authorization'] = token
+    }
+
+    return request
+})
 
 export default client;
