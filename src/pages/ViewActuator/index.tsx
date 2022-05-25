b2ed74d466e0a790d3537d6c223def621fe02bc2
Erick Spinelli Pimentel <erickspinellipimentel@gmail.com>
2022-05-25 20:42:37 -0300

    refactor: ViewActuator

--- src/pages/ViewActuator/index.tsx
+++ src/pages/ViewActuator/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useParams } from "react-router-dom";
-import { useQuery } from "react-query";
+import { useMutation, useQuery } from "react-query";
 
 import { ReactComponent as ActuatorIcon } from '@images/actuator.svg';
 
@@ -11,20 +11,32 @@ import ShortHeader from "../../components/ShortHeader";
 
 import { ReactComponent as UnknownTypeIcon } from '@images/unknown-type.svg';
 
-import { default as ActuatorType } from "../../types/Actuator";
+import Actuator, { default as ActuatorType } from "../../types/Actuator";
 import { getDevice } from "../../services/devices/getDevice";
 import ActuatorForm from "../../components/ActuatorForm";
 import { getActuator } from "../../services/actuators/getActuator";
+import { createActuator } from "../../services/actuators/createActuator";
+import { updateActuator } from "../../services/actuators/updateActuator";
 
 type Props = {
 
 }
+
+type StructedFormData = { name: string, type: string, port: number }
 
 export default function ViewActuator({ }: Props) {
     const { actuatorId = "" } = useParams()
     const { isLoading, data: actuator} = useQuery(["sensor", actuatorId], () => getActuator(actuatorId))
     const { isLoading: isLoadingDevice, data: device } = useQuery(["device", actuator?.deviceId], () => actuator && actuator.deviceId ? getDevice(actuator.deviceId) : undefined)
 
+    const {mutate: saveSensor} = useMutation(
+        (event: StructedFormData) => {
+            return updateActuator(new Actuator({
+                ...event
+            }))
+        }
+    )
+    
     const pageTitle = isLoading || !actuator ? "Loading..." : actuator.name
 
     const submitForm = (data: { name: string, port: number | string }) => {
--- src/services/actuators/updateActuator.ts
+++ src/services/actuators/updateActuator.ts
@@ -2,8 +2,6 @@ import client from "../../axios/client";
 import resources from '../../constants/resources'
 import Actuator from "../../types/Actuator";
 
-export function updateActuator(actuatorId: string): Promise<Actuator> {
-    return client.get(resources.UPDATE_ACTUATOR.replace('$actuatorId', actuatorId)).then(({ data: actuator }: { data: Actuator }) => {
-        return actuator;
-    });
+export function updateActuator(actuator: Actuator): Promise<Actuator> {
+    return client.put(resources.GET_ACTUATOR.replace('$actuatorId', actuator.id!), {...actuator})
 }
