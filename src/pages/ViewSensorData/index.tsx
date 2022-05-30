2b0740ab18cffe1c27a19e635a742158084c7a99
Isaque Melo <isaque.melo@mercadolivre.com>
2022-05-30 20:31:09 -0300

    feat: Add no sensor data message

--- src/pages/ViewSensorData/index.tsx
+++ src/pages/ViewSensorData/index.tsx
@@ -3,12 +3,14 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useMutation, useQuery, useQueryClient } from "react-query";
 
 import { ReactComponent as DatabaseIcon } from '@images/database.svg';
+import { ReactComponent as BoxIcon } from '@images/box.svg';
 
 import "./style.scss";
 
 import ShortHeader from "../../components/ShortHeader";
 import { getSensor } from "../../services/sensors/getSensor";
 import { getSensorData } from "../../services/sensors/getSensorData";
+import Typography from "../../components/Typography";
 
 
 type Props = {
@@ -36,27 +38,43 @@ export default function ViewSensorData({ }: Props) {
 
             <div className="container page">
                 <h3>Data</h3>
-                <ul className="sensor-data">
-                    <li>
-                        <span>Value</span>
-                        <span>Time</span>
-                    </li>
-                    {sensorData && sensorData.map(data => {
-                        const date = new Date(data.createdAt)
-                        return (
-                            <li>
-                                <span>{data.data.toString()}</span>
-                                <div></div>
-                                <span>{`${date.getHours()}h${date.getMinutes()}`}</span>
-                            </li>
-                        )
-                    })}
-                </ul>
 
-                <div className="controls">
-                    <button onClick={() => page - 1 > 0 ? setPage(page - 1) : ""}>&lt;</button>
-                    <button onClick={() => setPage(page + 1)}>&gt;</button>
-                </div>
+                {sensorData && sensorData.length === 0 && (
+                    <div className="no-results">
+                        <BoxIcon />
+                        <Typography type="body" alignment="center" size="l">Nothing here yet</Typography>
+                        <Typography type="body" alignment="center" size="m">If this is unexpected, check if your sensor is properly configured.</Typography>
+                    </div>
+                )}
+
+                {sensorData && sensorData.length >= 1 && (
+                    <>
+                        <ul className="sensor-data">
+                            <li>
+                                <span>Value</span>
+                                <span>Time</span>
+                            </li>
+                            {sensorData && sensorData.map(data => {
+                                const date = new Date(data.createdAt)
+                                return (
+                                    <li>
+                                        <span>{data.data.toString()}</span>
+                                        <div></div>
+                                        <span>{`${date.getHours()}h${date.getMinutes()}`}</span>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+
+                        <div className="controls">
+                            <button onClick={() => page - 1 > 0 ? setPage(page - 1) : ""}>&lt;</button>
+                            <button onClick={() => setPage(page + 1)}>&gt;</button>
+                        </div>
+                    </>
+                )}
+
+
+
             </div>
 
         </div>
