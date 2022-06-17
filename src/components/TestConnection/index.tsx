403d42c6889abe0544a7adcf1a252e56f614d079
Isaque Melo <isaque.melo@mercadolivre.com>
2022-06-17 18:28:47 -0300

    refactor: Replace xfetch with axios defaults

--- src/components/TestConnection/index.tsx
+++ src/components/TestConnection/index.tsx
@@ -7,7 +7,6 @@ import "./style.scss";
 import wifiIcon from '@images/wifi-icon.svg'
 import Typography from "../Typography";
 import Button from "../Button";
-import xfetch from "../../helpers/xfetch";
 
 type Props = {
     nextStep: Function,
@@ -20,43 +19,14 @@ export default function TestConnection({ nextStep }: Props) {
 
     const testConnection = () => {
         setIsTestingConnection(true);
-        alert("A popup will be open to comunicate with the device, keep it open")
 
-        // @ts-ignore
-        window.popup = window.open("http://192.168.4.1/server.html", '_blank', 'toolbar=no,titlebar=no,status=no,menubar=no,scrollbars=no,resizable=no,left=12000, top=12000,width=10,height=10,visible=none');
-
-        let success = false
-
-        // Awaits until device is fully loaded
-        setTimeout(() => {
-            window.focus()
-            xfetch('http://192.168.4.1/ping').then(() => {
-                success = true
-                nextStep()
-            }).catch(() => {
-                alert("Sorry, we could not validate your connection to the device.")
-            }).finally(() => {
-                setIsTestingConnection(false);
-            })
-        }, 2000)
-
-        // Detects if the state success state hasnt changed
-        setTimeout(() => {
-            if (!success) {
-                alert("Sorry, we could not validate your connection to the device.")
-                setIsTestingConnection(false);
-                // @ts-ignore
-                window.popup.close()
-            }
-        }, 5000)
-
-        // axios.get(`http://192.168.4.1/ping`, { timeout: 5000 }).then(() => {
-        //     nextStep()
-        // }).catch(() => {
-        //     alert("Sorry, we could not validate your connection to the device.")
-        // }).finally(() => {
-        //     setIsTestingConnection(false);
-        // })
+        axios.get(`http://192.168.4.1/ping`, { timeout: 5000 }).then(() => {
+            nextStep()
+        }).catch(() => {
+            alert("Sorry, we could not validate your connection to the device.")
+        }).finally(() => {
+            setIsTestingConnection(false);
+        })
     }
 
     return (
